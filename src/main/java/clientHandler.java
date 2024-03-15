26f1bf181aeadcc3fc0d3d1dc6f41b25f8f5cb41
AtheerAlhammami1 <a.alhammami@wakeb.tech>
2024-03-16 02:56:25 +0300

    extract path and return with body

--- src/main/java/clientHandler.java
+++ src/main/java/clientHandler.java
@@ -14,18 +14,39 @@ public class clientHandler extends Thread{
 //            Read from client
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(clientSocket.getInputStream()));
-            boolean isAcceptedPath = pathAccepted(reader.readLine());
-            if(isAcceptedPath){
-                clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                return;
-            }
-            clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+            String path = pathParser(reader.readLine());
+            String response = "HTTP/1.1 200 OK\r\n".concat("Content-Type: text/plain\r\n")
+                    .concat(String.format("Content-Length: %d \r\n", path.length())).concat(String
+                            .format("\r\n %s \r\n", path));
+            System.out.println(response);
+             clientSocket.getOutputStream().write(response.getBytes());
+
+//            boolean isAcceptedPath = pathAccepted(reader.readLine());
+//            if(isAcceptedPath){
+//                clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+//                return;
+//            }
+//            clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
+    private String pathParser(String requestLine){
+        String pattern = "^(\\S+)\\s+(/(\\S+)).*";
+
+        Pattern regex = Pattern.compile(pattern);
+        Matcher matcher = regex.matcher(requestLine);
+        if(!matcher.matches()){
+            System.out.println("INVALID REQUEST");
+            return "undefined";
+        }
+        String fullPath = matcher.group(2).substring(1);
+        String[] pathParts = fullPath.split("/",2);
+        System.out.println(pathParts[1]);
+        return pathParts[1];
+    }
     private boolean pathAccepted(String requestLine){
         String pattern = "^(\\S+)\\s+(/\\S*).*";
         Pattern regex = Pattern.compile(pattern);
