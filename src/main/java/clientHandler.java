fcba214fdaf13cf502d7cb02eb39d420440b2b3e
AtheerAlhammami1 <a.alhammami@wakeb.tech>
2024-03-16 03:08:44 +0300

    fix 404 commented

--- src/main/java/clientHandler.java
+++ src/main/java/clientHandler.java
@@ -14,19 +14,21 @@ public class clientHandler extends Thread{
 //            Read from client
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(clientSocket.getInputStream()));
-            String path = pathParser(reader.readLine());
-            String response = "HTTP/1.1 200 OK\r\n".concat("Content-Type: text/plain\r\n")
-                    .concat(String.format("Content-Length: %d \r\n", path.length())).concat(String
-                            .format("\r\n %s \r\n", path));
-            System.out.println(response);
-             clientSocket.getOutputStream().write(response.getBytes());
+            String req = reader.readLine();
+            String path;
+            String response;
 
-//            boolean isAcceptedPath = pathAccepted(reader.readLine());
-//            if(isAcceptedPath){
-//                clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-//                return;
-//            }
-//            clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+            boolean isAcceptedPath = pathAccepted(req);
+            if(isAcceptedPath){
+                path = pathParser(req);
+                response = "HTTP/1.1 200 OK\r\n".concat("Content-Type: text/plain\r\n")
+                        .concat(String.format("Content-Length: %d \r\n", path.length())).concat(String
+                                .format("\r\n %s \r\n", path));
+                System.out.println(response);
+                clientSocket.getOutputStream().write(response.getBytes());
+                return;
+            }
+            clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
 
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -40,7 +42,7 @@ public class clientHandler extends Thread{
         Matcher matcher = regex.matcher(requestLine);
         if(!matcher.matches()){
             System.out.println("INVALID REQUEST");
-            return "undefined";
+            return "";
         }
         String fullPath = matcher.group(2).substring(1);
         String[] pathParts = fullPath.split("/",2);
@@ -57,7 +59,7 @@ public class clientHandler extends Thread{
             return false;
         }
         String path = matcher.group(2);
-        if(!path.equals("/")){
+        if(!path.equals("/") && !path.contains("/echo")){
             return false;
         }
         return true;
