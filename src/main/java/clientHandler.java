f4b790b55c60086ce62f041c92a6b81d353b7c14
AtheerAlhammami1 <a.alhammami@wakeb.tech>
2024-03-16 14:34:06 +0300

    parse header

--- src/main/java/clientHandler.java
+++ src/main/java/clientHandler.java
@@ -15,29 +15,58 @@ public class clientHandler extends Thread{
             BufferedReader reader = new BufferedReader(
                     new InputStreamReader(clientSocket.getInputStream()));
             String req = reader.readLine();
-            String path;
-            String response;
+            String response=getResponse("");
 
-            boolean isAcceptedPath = pathAccepted(req);
-            if(isAcceptedPath){
-                path = pathParser(req);
-                response = "HTTP/1.1 200 OK\r\n".concat("Content-Type: text/plain\r\n")
-                        .concat(String.format("Content-Length: %d\r\n", path.length())).concat(String
-                                .format("\r\n%s\r\n", path));
-                System.out.println(response);
-                clientSocket.getOutputStream().write(response.getBytes());
+            String receivedPath = getPath(req);
+            boolean isAcceptedPath = isPathAccepted(receivedPath);
+            if(!isAcceptedPath) {
+                clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
                 return;
             }
-            clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+
+            if(receivedPath.contains("/echo")){
+                response = getResponse(getPathTail(req));
+            }else if(receivedPath.contains("/user-agent")){
+                response = getResponse(extractHeader(reader, "User-Agent"));
+            }
+            System.out.println(response);
+            clientSocket.getOutputStream().write(response.getBytes());
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
+    private String getResponse(String body){
+        return "HTTP/1.1 200 OK\r\n".concat("Content-Type: text/plain\r\n")
+                .concat(String.format("Content-Length: %d\r\n", body.length())).concat(String
+                        .format("\r\n%s\r\n", body));
+    }
 
-    private String pathParser(String requestLine){
+    private String extractHeader(BufferedReader reader, String target){
+        String header="";
+        String targetValue="";
+        try {
+            String line = reader.readLine();
+            while(line != null){
+                if(line.contains(target)){
+                    header = line;
+                    break;
+                }
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        int startIndex = header.indexOf(target);
+        if (startIndex != -1) {
+            startIndex += target.length() + 2; // Skip "User-Agent:" and the following space and colon
+             targetValue = header.substring(startIndex);
+            System.out.println("User-Agent: " + targetValue);
+        }
+        return targetValue;
+    }
+    private String getPathTail(String requestLine){
         String pattern = "^(\\S+)\\s+(/(\\S+)).*";
-
         Pattern regex = Pattern.compile(pattern);
         Matcher matcher = regex.matcher(requestLine);
         if(!matcher.matches()){
@@ -49,19 +78,19 @@ public class clientHandler extends Thread{
         System.out.println(pathParts[1]);
         return pathParts[1];
     }
-    private boolean pathAccepted(String requestLine){
+    private boolean isPathAccepted(String path){
+        return path.equals("/") || path.contains("/echo") || path.contains("/user-agent");
+    }
+
+    private String getPath(String requestLine){
         String pattern = "^(\\S+)\\s+(/\\S*).*";
         Pattern regex = Pattern.compile(pattern);
         Matcher matcher = regex.matcher(requestLine);
 
         if(!matcher.matches()){
             System.out.println("INVALID REQUEST");
-            return false;
+            return "invalid";
         }
-        String path = matcher.group(2);
-        if(!path.equals("/") && !path.contains("/echo")){
-            return false;
-        }
-        return true;
+        return matcher.group(2);
     }
 }
